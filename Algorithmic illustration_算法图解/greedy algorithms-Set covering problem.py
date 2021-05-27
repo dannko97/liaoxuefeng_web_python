9a5d527658231b51f476ea108b70f013595f05ac
yuyang <531480035@qq.com>
2021-05-27 20:51:27 +0800

    greedy

---
+++ Algorithmic illustration_算法图解/greedy algorithms-Set covering problem.py
@@ -0,0 +1,7 @@
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+final_stations = set()
